790bd5c3393b8ff697109674e1de7be2255d2f19
thiagoferreira47691 <thiagoferreira47691@gmail.com>
2025-12-25 05:43:05 +0000

    db hospedado

--- model/db.js
+++ model/db.js
@@ -3,8 +3,12 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 }); 
 module.exports = { pool };
